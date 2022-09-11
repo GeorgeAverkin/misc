2ae569c4dd1c10bd821f38b0574a88fb92663e66
George Averkin <nrg3125@gmail.com>
2022-09-11 22:04:08 +0300

    Update 2.dictionary.goo.ne.jp.js

--- 2.dictionary.goo.ne.jp.js
+++ 2.dictionary.goo.ne.jp.js
@@ -17,48 +17,84 @@ function parse_meaning(text, newline) {
   const chars = [...text]
   
   chars.forEach(char => {
-    if (DELIMITERS.includes(char) && buf.length) {
+    const is_newline = DELIMITERS.includes(char)
+
+    if (is_newline && buf.length) {
       buf.push(newline)
     }
     buf.push(char)
+    
+    if (is_newline) {
+      buf.push('&nbsp;')
+    }
   })
   
   const formatted_text = buf.join('')
   return formatted_text
 }
 
-function get_meaning(newline) {
-  const meaning_blocks = document.getElementsByClassName('meaning_block')
-  
-  if (meaning_blocks.length !== 1) {
-    throw new Error()
-  }
-  const meaning_block = meaning_blocks[0]
-  const meaning_p_list = meaning_block.querySelectorAll('.block > .inner_block > p')
-  
-  if (meaning_p_list.length !== 1) {
-    throw new Error()
-  }
-  const meaning_p = meaning_p_list[0]
-  const meaning = parse_meaning(meaning_p.innerHTML, newline)
-  return meaning
-}
 
-function get_readings() {
-  const dl_list = document.querySelectorAll('.reads > dl')
-  
-  if (dl_list.length !== 1) {
-    throw new Error()
+class KankenDom {
+  static get_meaning(newline) {
+    const meaning_blocks = document.getElementsByClassName('meaning_block')
+
+    if (meaning_blocks.length !== 1) {
+      throw new Error()
+    }
+    const meaning_block = meaning_blocks[0]
+    const meaning_p_list = meaning_block.querySelectorAll('.block > .inner_block > p')
+
+    if (meaning_p_list.length !== 1) {
+      throw new Error()
+    }
+    const meaning_p = meaning_p_list[0]
+    const meaning = parse_meaning(meaning_p.innerHTML, newline)
+    return meaning
   }
-  const dl = dl_list[0]
-  
-  if (dl.children.length !== 4) {
-    throw new Error()
+
+  static get_readings() {
+    const dl_list = document.querySelectorAll('.reads > dl')
+
+    if (dl_list.length !== 1) {
+      throw new Error()
+    }
+    const dl = dl_list[0]
+
+    if (dl.children.length !== 4) {
+      throw new Error()
+    }
+    const [on_label, on_data, kun_label, kun_data] = dl.children
+    const on_html = on_data.innerHTML.trim()
+    const kun_html = kun_data.innerHTML.trim()
+    return `【音】${on_html}【訓】${kun_html}`
   }
-  const [on_label, on_data, kun_label, kun_data] = dl.children
-  const on_html = on_data.innerHTML.trim()
-  const kun_html = kun_data.innerHTML.trim()
-  return `【音】${on_html}【訓】${kun_html}`
+
+  static get_info() {
+    const dl_list = document.querySelectorAll('.info > dl')
+    
+    if (dl_list.length !== 1) {
+      throw new Error()
+    }
+    const dl = dl_list[0]
+    
+    
+    if (dl.children.length !== 8) {
+      throw new Error()
+    }
+    const [
+      busyu_label,
+      busyu_data,
+      kakusuu_label,
+      kakusuu_data,
+      syubetsu_label,
+      syubetsu_data,
+      kanken_label,
+      kanken_data,
+    ] = dl.children
+    
+    const kanken_html = kanken_data.innerHTML.trim()
+    return `【${kanken_html}】`
+  }
 }
 
 function write_to_log() {
@@ -69,10 +105,11 @@ function write_to_log() {
 }
 
 function format_text() {
-  const meaning = get_meaning('<br/>')
-  const readings = get_readings()
+  const meaning = KankenDom.get_meaning('<br/>')
+  const readings = KankenDom.get_readings()
+  const info = KankenDom.get_info()
   const element = document.querySelector('.meaning_block .block')
-  element.innerHTML = `検定${readings}<br/>${meaning}`
+  element.innerHTML = `検定${info}${readings}<br/>${meaning}`
 }
 
 function add_button() {
